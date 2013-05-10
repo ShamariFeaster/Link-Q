c5a2c9ad34c5f62e81aca497e9bd2576585a4f88
Shamari Feaster <sf11@my.fsu.edu>
2013-05-10 16:34:32 -0400

    Initial Commit

---
+++ popup.js
@@ -0,0 +1,10 @@
+(function() {
+   var linqs = document.getElementById('linqs');
+	console.log(linqs);
+	console.log('hello');
+	for(var i = 0; i < pages.length; i++) {
+		console.log(pages[i].text + ' ' + pages[i].url);
+	}
+
+})();
+//newText = '<a href="'+url+'">'+text+'</a><br>';
